05acca6a1c614f54daf9e0192ed1a2e61d2848ce
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-16 09:58:50 +0530

    s

--- routes/ReqCrm.js
+++ routes/ReqCrm.js
@@ -1,68 +1,110 @@
-const express = require("express");
-const router = express.Router();
-const { usersModel } = require("../dbSchemaReq");
+var express = require('express');
+var router = express.Router();
+const {mongoose,usersModel} = require('../dbSchemaReq')
+const {mongodb,dbName,dbUrl} = require('../dbConfig')
+const {createToken,jwtDecode,validate,roleAdmin} = require('../auth')
 
-// Route to get all requirements
-router.get("/get", async (req, res) => {
-  try {
-    const users = await usersModel.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+mongoose.connect(dbUrl)
 
-// Route to get the number of requirements
-router.get("/getReqLength", async (req, res) => {
-  try {
-    const userCount = await usersModel.countDocuments();
-    res.status(200).json({ reqCount: userCount });
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 
-// Route to add a new requirement
-router.post("/ReqAdd", async (req, res) => {
+router.get('/get',async(req,res)=>{
+  
+    let users = await usersModel.find()
+      res.send({
+        statusCode:200,
+        data:users
+      })
+})
+
+
+
+router.get('/getReqLength',async(req,res)=>{
+  
+  let users = await usersModel.find()
+    res.send({
+      statusCode:200,
+      data:users.length
+    })
+})
+
+router.post('/ReqAdd', async(req,res)=>{
   try {
-    const existingUser = await usersModel.findOne({ email: req.body.email });
-    if (existingUser) {
-      res.status(400).json({ message: "Requirement Already Exists" });
-    } else {
-      const newUser = await usersModel.create(req.body);
-      res.status(200).json({ message: "Requirement added Successfully" });
+    let user = await usersModel.find({email:req.body.email})
+    if(user.length)
+    {
+      res.send({
+        statusCode:400,
+        message:"Req Already Exists"
+      })
+    }
+    else{
+      let newUser = await usersModel.create(req.body)
+      res.send({
+        statusCode:200,
+        message:"Req added Successfull"
+      })
     }
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    console.log(error)
+    res.send({statusCode:200,message:"Internal Server Error",error})
   }
-});
+})
 
-// Route to delete a requirement by ID
-router.delete("/delete-Req/:id", async (req, res) => {
-  try {
-    const deletedUser = await usersModel.findByIdAndDelete(req.params.id);
-    if (deletedUser) {
-      res.status(200).json({ message: "User deleted successfully" });
-    } else {
-      res.status(400).json({ message: "User does not exist" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 
-// Route to edit a requirement by ID
-router.put("/edit-Req/:id", async (req, res) => {
+router.delete('/delete-Req/:id',async(req,res)=>{
   try {
-    const updatedUser = await usersModel.findByIdAndUpdate(req.params.id, req.body);
-    if (updatedUser) {
-      res.status(200).json({ message: "User data saved successfully" });
-    } else {
-      res.status(400).json({ message: "User does not exist" });
-    }
+    let user = await usersModel.find({_id:mongodb.ObjectId(req.params.id)})
+    if(user.length)
+    {
+       let users =await usersModel.deleteOne({_id:mongodb.ObjectId(req.params.id)})
+      res.send({statusCode:200,message:"User deleted successfully"})
+      }
+    else
+      res.send({statusCode:400,message:"User does not exists"})
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    console.log(error)
+    res.send({statusCode:400,message:"Internal Server Error",error})
   }
-});
+})
+
+router.get('/edit-Req/:id',async(req,res)=>{
+  try {
+    let user = await usersModel.findOne({_id:mongodb.ObjectId(req.params.id)})
+    console.log(user)
+    if(user)
+    {
+      
+      res.send(user)
+      }
+    else
+      res.send({statusCode:400,message:"User does not exists"})
+  } catch (error) {
+    console.log(error)
+    res.send({statusCode:400,message:"Internal Server Error",error})
+  }
+})
+ 
+router.put('/edit-Req/:id',async(req,res)=>{
+  try {
+    let user = await usersModel.findOne({_id:mongodb.ObjectId(req.params.id)})
+    console.log(user)
+    if(user)
+    {
+       user.reqName = req.body.reqName
+       user.company =req.body.company
+      user.email =req.body.email
+      user.mobileNumber =req.body.mobileNumber
+      user.status =req.body.status
+      await user.save()
+      res.send({statusCode:200,message:"User data saved successfully"})
+      }
+    else
+      res.send({statusCode:400,message:"User does not exists"})
+  } catch (error) {
+    console.log(error)
+    res.send({statusCode:400,message:"Internal Server Error",error})
+  }
+})
+
 
 module.exports = router;
